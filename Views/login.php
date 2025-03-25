0df016b509ca76fd31d0976257bdd0459a4e78cd
Cecile Lemaire <cecile.lemaire@grow-your-business.fr>
2025-03-25 16:11:59 +0100

    fix : login

---
+++ Views/login.php
@@ -0,0 +1,15 @@
+<form action="" method="post">
+ <div>
+     <label for="username">Entrer votre identifiant: </label>
+     <input type="text" name="username" id="username" required/>
+ </div>
+ <div>
+     <label for="password">Entrer votre mot de passe: </label>
+     <input type="password" name="password" id="password" required />
+ </div>
+ <div>
+     <input type="submit" value="Se connecter"/>
+ </div>
+ </form>
+ 
+ <?php
