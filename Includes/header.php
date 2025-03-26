3516d4611ebf994dd031407052dcf5c4fa72cd31
Inès <ines.enrique@icloud.com>
2025-03-26 09:37:28 +0100

    ajout du bouton de déconnexion

--- Includes/header.php
+++ Includes/header.php
@@ -1,8 +1,8 @@
 <nav class="menu">
   <ul>
     <li><a href="#">Logo</a></li>
-    <li><a href="#">Dashboard</a></li>
-    <li><a href="#">Chatbot</a></li>
-    <li><a href="#">Se déconnecter</a></li>
+    <li><a href="">Dashboard</a></li>
+    <li><a href="">Chatbot</a></li>
+    <li><a href="<?= URL ?>logout">Se déconnecter</a></li>
   </ul>
 </nav>
