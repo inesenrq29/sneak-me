3b0810efa620bb6bb032c8b0e4affafec96ee1b2
Inès <ines.enrique@icloud.com>
2025-03-26 10:31:36 +0100

    ajout du captcha

--- Views/login.php
+++ Views/login.php
@@ -1,13 +1,34 @@
+<?php
+// Création d'un code aléatoire pour le captcha si il n'existe pas encore dans la session
+if (!isset($_SESSION['captcha'])) {
+    $_SESSION['captcha'] = rand(10000, 99999);
+}
+$captcha = $_SESSION['captcha'];
+?>
+
 <form action="" method="post">
-<div>
-    <label for="username">Entrer votre identifiant: </label>
-    <input type="text" name="username" id="username" required/>
-</div>
-<div>
-    <label for="password">Entrer votre mot de passe: </label>
-    <input type="password" name="password" id="password" required />
-</div>
-<div>
-    <input type="submit" value="Se connecter"/>
-</div>
+    <div>
+        <label for="username">Entrer votre identifiant: </label>
+        <input type="text" name="username" id="username" required />
+    </div>
+    <div>
+        <label for="password">Entrer votre mot de passe: </label>
+        <input type="password" name="password" id="password" required />
+    </div>
+    <div>
+        <label for="captcha">Saisir le captcha: </label>
+        <input type="text" name="captcha" id="captcha" required />
+    </div>
+    <div>
+        <input type="submit" value="Se connecter" />
+    </div>
+    <p>Captcha: <?php echo $captcha; ?></p>
 </form>
+
+<?php
+// Affichage de l'erreur si le captcha ou les identifiants sont incorrects
+if (isset($_SESSION['error'])) {
+    echo '<p style="color:red;">' . $_SESSION['error'] . '</p>';
+    unset($_SESSION['error']);
+}
+?>
