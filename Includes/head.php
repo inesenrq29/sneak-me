45c3d436ba29d292c99fcbb47fcede5ee5696347
Cecile Lemaire <cecile.lemaire@grow-your-business.fr>
2025-03-28 13:28:04 +0100

    fix : couleur title

--- Includes/head.php
+++ Includes/head.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
     <link rel="stylesheet" href="<?= URL ?>Public/assets/css/header.css">
     <link rel="stylesheet" type="text/css" href="<?= URL ?>Public/assets/css/dashboard.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="<?= URL ?>Public/assets/css/chatbot.css">
 
     <title>Admin - SneakMe</title>
 </head>
+
