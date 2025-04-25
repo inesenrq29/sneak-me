d2b3353959134bfd59f73b1919c29372cd676c78
Cecile Lemaire <cecile.lemaire@grow-your-business.fr>
2025-04-25 14:32:44 +0200

    git ckec

--- Views/login.php
+++ Views/login.php
@@ -6,36 +6,45 @@ if (!isset($_SESSION['captcha'])) {
 $captcha = $_SESSION['captcha'];
 ?>
 
-<form action="" method="post">
-
-<div class="red">
-    <label for="username">Entrer votre identifiant: </label>
-    <input type="text" name="username" id="username" required/>
-</div>
-
-<div hidden>
-    <label for="lastname">Entrer votre nom: </label>
-    <input type="text" name="honeypot"/>
-</div>
-<div>
-    <label for="password">Entrer votre mot de passe: </label>
-    <input type="password" name="password" id="password" required />
-</div>
-   <div>
-        <label for="captcha">Saisir le captcha: </label>
-        <input type="text" name="captcha" id="captcha" required />
+<div class="login-container">
+    <div class="logo-container">
+        <img src="<?= URL ?>Public/images/logo-blue.png" alt="SneakMe Logo" class="login-logo">
     </div>
-<div class="connect">
-    <input type="submit" value="Se connecter"/>
-</div>
-    <p>Captcha: <?php echo $captcha; ?></p>
-</form>
+    <form action="" method="post">
+        <div class="form-group">
+            <label for="username">Entrer votre identifiant :</label>
+            <input type="text" name="username" id="username" required/>
+        </div>
 
-<?php
-// Affichage de l'erreur si le captcha ou les identifiants sont incorrects
-if (isset($_SESSION['error'])) {
-    echo '<p style="color:red;">' . $_SESSION['error'] . '</p>';
-    unset($_SESSION['error']);
-}
-?>
+        <div hidden>
+            <label for="lastname">Entrer votre nom :</label>
+            <input type="text" name="honeypot"/>
+        </div>
+
+        <div class="form-group">
+            <label for="password">Entrer votre mot de passe :</label>
+            <input type="password" name="password" id="password" required />
+        </div>
+
+        <div class="form-group">
+            <label for="captcha">Saisir le captcha :</label>
+            <input type="text" name="captcha" id="captcha" required />
+            <div class="captcha-container">
+                <?php echo $captcha; ?>
+            </div>
+        </div>
+
+        <div class="connect">
+            <input type="submit" value="Se connecter"/>
+        </div>
+    </form>
+
+    <?php
+    // Affichage de l'erreur si le captcha ou les identifiants sont incorrects
+    if (isset($_SESSION['error'])) {
+        echo '<div class="error-message">' . $_SESSION['error'] . '</div>';
+        unset($_SESSION['error']);
+    }
+    ?>
+</div>
 
